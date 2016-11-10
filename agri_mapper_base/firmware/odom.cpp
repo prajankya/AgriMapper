536747edf89892e94cc754cd432dead89d1cd72a
prajankya <prajankya@gmail.com>
2016-11-10 13:26:46 +0530

    Added odom class

--- agri_mapper_base/firmware/odom.cpp
+++ agri_mapper_base/firmware/odom.cpp
@@ -1,10 +1,36 @@
 #include "odom.h"
 #include <Arduino.h>
 
-void odom::init(ros::NodeHandle _nh, const char * topic){
+void odom::init(ros::NodeHandle _nh, const char * topic, int e1A, int e1B, int e2A, int e2B){
         nh = _nh;
 
         ros::Publisher odom_pub_(topic, &odom_msg);
         odom_pub = &odom_pub_;
         nh.advertise(*odom_pub);
+        left.init(e1A, e1B);
+        right.init(e2A, e2B);
+
+        oldL = 0;
+        oldR = 0;
+}
+
+void odom::loop(){
+        left.loop();
+        right.loop();
+        unsigned long nL = left.getPos();
+        unsigned long nR = right.getPos();
+
+        if(nL != oldL || nR != oldR) {
+                char l[10];
+                dtostrf(nL, 6, 2, l);
+
+                char r[10];
+                dtostrf(nR, 6, 2, r);
+
+                String s = String(l) + "," + String(r);
+                char bb[50];
+                s.toCharArray(bb, 50);
+                odom_msg.data = bb;
+                odom_pub->publish(&odom_msg);
+        }
 }
--- agri_mapper_base/firmware/odom.h
+++ agri_mapper_base/firmware/odom.h
@@ -5,15 +5,20 @@
 #include<ros.h>
 #include<ros/time.h>
 #include<std_msgs/String.h>
+#include "encoder.h"
 
 class odom {
   private:
     ros::NodeHandle nh;
     ros::Publisher * odom_pub;
     std_msgs::String odom_msg;
+    Encoder left;
+    Encoder right;
+    unsigned long oldL;
+    unsigned long oldR;
 
   public:
-    void init(ros::NodeHandle _nh, const char * topic);
+    void init(ros::NodeHandle _nh, const char * topic, int e1A, int e1B, int e2A, int e2B);
     void loop();
 };
 #endif
