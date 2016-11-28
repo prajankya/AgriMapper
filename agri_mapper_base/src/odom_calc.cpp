2e910a22ff487f81e0dfdc2d3c94a46c0f96877b
Prajankya <prajankya@gmail.com>
2016-11-28 16:52:13 +0530

    Editing odom

--- agri_mapper_base/src/odom_calc.cpp
+++ agri_mapper_base/src/odom_calc.cpp
@@ -28,6 +28,10 @@ double x = 0.0;
 double y = 0.0;
 double th = 0.0;
 
+double vx = 0.0;
+double vy = 0.0;
+double vth = 0.0;
+
 double string_to_double( const std::string& s ){
         std::istringstream i(s);
         double x;
@@ -78,9 +82,9 @@ void odomCallback(const std_msgs::String::ConstPtr & msg){
         double dy = l - (l * cos(dth));
 
         double dt = (current_time - last_time).toSec();
-        double vx = dx / dt;
-        double vy = dy / dt;
-        double vth = dth / dt;
+        vx = dx / dt;
+        vy = dy / dt;
+        vth = dth / dt;
 
         x += dx;
         y += dy;
@@ -91,43 +95,6 @@ void odomCallback(const std_msgs::String::ConstPtr & msg){
         }else if(th < 2 * PI) {
                 th = 0;
         }
-
-        //since all odometry is 6DOF we'll need a quaternion created from yaw
-        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
-
-        //first, we'll publish the transform over tf
-        geometry_msgs::TransformStamped odom_trans;
-        odom_trans.header.stamp = current_time;
-        odom_trans.header.frame_id = "odom";
-        odom_trans.child_frame_id = "chassis_link";
-
-        odom_trans.transform.translation.x = x;
-        odom_trans.transform.translation.y = y;
-        odom_trans.transform.translation.z = 0.0;
-        odom_trans.transform.rotation = odom_quat;
-
-        //send the transform
-        tf::TransformBroadcaster odom_broadcaster;
-        odom_broadcaster.sendTransform(odom_trans);
-
-        //next, we'll publish the odometry message over ROS
-        nav_msgs::Odometry odom;
-        odom.header.stamp = current_time;
-        odom.header.frame_id = "odom";
-
-        //set the position
-        odom.pose.pose.position.x = x;
-        odom.pose.pose.position.y = y;
-        odom.pose.pose.position.z = 0.0;
-        odom.pose.pose.orientation = odom_quat;
-
-        //set the velocity
-        odom.child_frame_id = "base_link";
-        odom.twist.twist.linear.x = vx;
-        odom.twist.twist.linear.y = vy;
-        odom.twist.twist.angular.z = vth;
-
-        odom_pub.publish(odom);
 
         last_time = current_time;
 }
@@ -142,7 +109,47 @@ int main(int argc, char** argv){
         current_time = ros::Time::now();
         last_time = ros::Time::now();
 
+        ros::Rate rate(10);
+
         while(n.ok()) {
+                //since all odometry is 6DOF we'll need a quaternion created from yaw
+                geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
+
+                //first, we'll publish the transform over tf
+                geometry_msgs::TransformStamped odom_trans;
+                odom_trans.header.stamp = current_time;
+                odom_trans.header.frame_id = "odom";
+                odom_trans.child_frame_id = "base_link";
+
+                odom_trans.transform.translation.x = x;
+                odom_trans.transform.translation.y = y;
+                odom_trans.transform.translation.z = 0.0;
+                odom_trans.transform.rotation = odom_quat;
+
+                //send the transform
+                tf::TransformBroadcaster odom_broadcaster;
+                odom_broadcaster.sendTransform(odom_trans);
+
+                //next, we'll publish the odometry message over ROS
+                nav_msgs::Odometry odom;
+                odom.header.stamp = current_time;
+                odom.header.frame_id = "odom";
+
+                //set the position
+                odom.pose.pose.position.x = x;
+                odom.pose.pose.position.y = y;
+                odom.pose.pose.position.z = 0.0;
+                odom.pose.pose.orientation = odom_quat;
+
+                //set the velocity
+                odom.child_frame_id = "base_link";
+                odom.twist.twist.linear.x = vx;
+                odom.twist.twist.linear.y = vy;
+                odom.twist.twist.angular.z = vth;
+
+                odom_pub.publish(odom);
                 ros::spinOnce();
+
+                rate.sleep();
         }
 }
