ceebfa4b94ebf96bffebecb76b81f66ffad4839b
prajankya <prajankya@gmail.com>
2017-01-05 22:12:19 +0530

    Added define.. USE_ROS

--- agri_mapper_base/firmware/firmware.cpp
+++ agri_mapper_base/firmware/firmware.cpp
@@ -1,19 +1,28 @@
-#include <ros.h>
+#define USE_ROS 1
+
+#ifdef USE_ROS
+  #include <ros.h>
+  #include <ros/time.h>
+  #include <std_msgs/String.h>
+#endif
+
 #include <Arduino.h>
-#include <ros/time.h>
-#include <std_msgs/String.h>
 #include "odom.h"
 
+#ifdef USE_ROS
 ros::NodeHandle nh;
 
 std_msgs::String odom_msg;
 ros::Publisher odom_pub("odom_pub", &odom_msg);
+#endif
 
 Odom odom;
 
 void setup() {
+#ifdef USE_ROS
   nh.initNode();
   nh.advertise(odom_pub);
+#endif
   odom.init(7, 8, 4, 6);
 }
 
@@ -30,9 +39,13 @@ void loop() {
     previousMillis = currentMillis;
 
     //nh.loginfo(odom.msg);
+#ifdef USE_ROS
     odom_msg.data = odom.msg;
     odom_pub.publish(&odom_msg);
+#endif
   }
 
+#ifdef USE_ROS
   nh.spinOnce();
+#endif
 }
