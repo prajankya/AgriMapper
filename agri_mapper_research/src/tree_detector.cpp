7e7b34a86379c828704e3ae02d56f3726b78b408
prajankya <prajankya@gmail.com>
2017-01-14 10:06:17 +0530

    Adding Hough circle transform

--- agri_mapper_research/src/tree_detector.cpp
+++ agri_mapper_research/src/tree_detector.cpp
@@ -7,7 +7,7 @@
 
 void mapSubCallback(const nav_msgs::OccupancyGridConstPtr& map);
 
-cv_bridge::CvImage cv_img;
+cv_bridge::CvImage cv_img, cv_circlesImg;
 ros::Publisher image_pub;
 
 int main(int argc, char **argv) {
@@ -19,6 +19,9 @@ int main(int argc, char **argv) {
 
   cv_img.header.frame_id = "map_image";
   cv_img.encoding = sensor_msgs::image_encodings::MONO8;
+
+  cv_circlesImg.header.frame_id = "map_circles";
+  cv_circlesImg.encoding = sensor_msgs::image_encodings::MONO8;
 
   while (n.ok()) {
     ros::spinOnce();
@@ -40,6 +43,8 @@ void mapSubCallback(const nav_msgs::OccupancyGridConstPtr& map) {
   if ( (map_mat->rows != size_y) && (map_mat->cols != size_x)) {
     *map_mat = cv::Mat(size_y, size_x, CV_8U);
   }
+
+  cv_circlesImg.image = cv::Mat(size_y, size_x, CV_8U);
 
   const std::vector<int8_t>& map_data(map->data);
 
@@ -72,7 +77,28 @@ void mapSubCallback(const nav_msgs::OccupancyGridConstPtr& map) {
     }
   }
 
+  // Reduce the noise so we avoid false circle detection
+  //cv::GaussianBlur(cv_img.image, cv_img.image, cv::Size(9, 9), 2, 2);
+
+  cv::vector<cv::Vec3f> circles;
+
+  /// Apply the Hough Transform to find the circles
+  HoughCircles(cv_circlesImg.image, circles, CV_HOUGH_GRADIENT, 1, cv_circlesImg.image.rows / 8, 200, 100, 0, 0);
+
+  ROS_INFO_STREAM("Circles Detected:" << circles.size());
+
+  /// Draw the circles detected
+  for (size_t i = 0; i < circles.size(); i++) {
+    cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+    int radius = cvRound(circles[i][2]);
+    // circle center
+    //cv::circle(cv_circlesImg.image, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
+    // circle outline
+    cv::circle(cv_circlesImg.image, center, radius, 255, 3, 8, 0);
+  }
+
   image_pub.publish(cv_img.toImageMsg());
+  circles_pub.publish(cv_circlesImg.toImageMsg());
 
   return;
 }
