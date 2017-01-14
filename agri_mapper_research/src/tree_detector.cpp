59cab3ac9585e8bdb1fb9f96b93e9361d71c571b
prajankya <prajankya@gmail.com>
2017-01-14 10:07:33 +0530

    Bug fixes

--- agri_mapper_research/src/tree_detector.cpp
+++ agri_mapper_research/src/tree_detector.cpp
@@ -9,6 +9,7 @@ void mapSubCallback(const nav_msgs::OccupancyGridConstPtr& map);
 
 cv_bridge::CvImage cv_img, cv_circlesImg;
 ros::Publisher image_pub;
+ros::Publisher circles_pub;
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "tree_detector");
@@ -16,6 +17,7 @@ int main(int argc, char **argv) {
   ros::NodeHandle n;
   ros::Subscriber scan_sub = n.subscribe("/map", 50, mapSubCallback);
   image_pub = n.advertise<sensor_msgs::Image>("image", 50);
+  circles_pub = n.advertise<sensor_msgs::Image>("circles_image", 50);
 
   cv_img.header.frame_id = "map_image";
   cv_img.encoding = sensor_msgs::image_encodings::MONO8;
@@ -78,16 +80,16 @@ void mapSubCallback(const nav_msgs::OccupancyGridConstPtr& map) {
   }
 
   // Reduce the noise so we avoid false circle detection
-  //cv::GaussianBlur(cv_img.image, cv_img.image, cv::Size(9, 9), 2, 2);
+  //cv::GaussianBlur(cv_circlesImg.image, cv_img.image, cv::Size(9, 9), 2, 2);
 
   cv::vector<cv::Vec3f> circles;
 
-  /// Apply the Hough Transform to find the circles
+  // Apply the Hough Transform to find the circles
   HoughCircles(cv_circlesImg.image, circles, CV_HOUGH_GRADIENT, 1, cv_circlesImg.image.rows / 8, 200, 100, 0, 0);
 
   ROS_INFO_STREAM("Circles Detected:" << circles.size());
 
-  /// Draw the circles detected
+  // Draw the circles detected
   for (size_t i = 0; i < circles.size(); i++) {
     cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
     int radius = cvRound(circles[i][2]);
