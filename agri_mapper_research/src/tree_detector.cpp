8924fd3116daa63c3eb001fade2e2b797d06849d
prajankya <prajankya@gmail.com>
2017-01-14 07:36:09 +0530

    Added subscriber to the tree detector

--- agri_mapper_research/src/tree_detector.cpp
+++ agri_mapper_research/src/tree_detector.cpp
@@ -1,12 +1,21 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 
+#include <nav_msgs/OccupancyGrid.h>
+
+void mapSubCallback(const nav_msgs::OccupancyGrid msg);
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "tree_detector");
 
   ros::NodeHandle n;
+  ros::Subscriber scan_sub = n.subscribe<nav_msgs::OccupancyGrid>("/map", 50, mapSubCallback);
 
   while (n.ok()) {
     ros::spinOnce();
   }
+}
+
+void mapSubCallback(const nav_msgs::OccupancyGrid msg) {
+  return;
 }
