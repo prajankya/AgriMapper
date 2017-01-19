cd81c033b8c106750a7d16eb67c53afaae10d410
Prajankya <prajankya@gmail.com>
2017-01-19 11:22:08 +0530

    bug fixes, renamed circles to detected

--- agri_mapper_research/src/tree_detector.cpp
+++ agri_mapper_research/src/tree_detector.cpp
@@ -7,9 +7,10 @@
 
 void mapSubCallback(const nav_msgs::OccupancyGridConstPtr& map);
 void detectTrees();
+void mapToImage();
 
-cv_bridge::CvImage cv_img, cv_circlesImg;
-ros::Publisher image_pub, circles_pub;
+cv_bridge::CvImage cv_img, cv_detectionImg;
+ros::Publisher image_pub, detected_pub;
 
 nav_msgs::OccupancyGrid map;
 
@@ -35,13 +36,13 @@ int main(int argc, char **argv) {
   ros::Subscriber scan_sub = n.subscribe("/map", 50, mapSubCallback);
 
   image_pub = n.advertise<sensor_msgs::Image>("image", 50);
-  circles_pub = n.advertise<sensor_msgs::Image>("circles_image", 50);
+  detected_pub = n.advertise<sensor_msgs::Image>("detection_image", 50);
 
   cv_img.header.frame_id = "map_image";
   cv_img.encoding = sensor_msgs::image_encodings::MONO8;
 
-  cv_circlesImg.header.frame_id = "map_circles";
-  cv_circlesImg.encoding = sensor_msgs::image_encodings::MONO8;
+  cv_detectionImg.header.frame_id = "map_detectedClustor";
+  cv_detectionImg.encoding = sensor_msgs::image_encodings::MONO8;
 
   ros::Rate rate(10);
 
@@ -62,6 +63,49 @@ void mapSubCallback(const nav_msgs::OccupancyGridConstPtr& map_) {
 }
 
 void detectTrees() {
+  mapToImage();
+
+
+  // Reduce the noise so we avoid false circle detection
+  //cv::GaussianBlur(cv_circlesImg.image, cv_img.image, cv::Size(9, 9), 2, 2);
+  /*
+     std::vector<cv::Vec3f> circles;
+
+     // Apply the Hough Transform to find the circles
+     HoughCircles(cv_img.image,
+                 circles,
+                 CV_HOUGH_GRADIENT,
+                 inverse_ratio_of_resolution,
+                 minimum_distance_between_detected_centers,
+                 upper_threshold_for_canny_detector,
+                 center_detection_threshold,
+                 min_radius,
+                 max_radius);
+
+     std::cout << "\nNo:" << circles.size() << " circles" << std::endl;
+
+     // Draw the circles detected
+     for (size_t i = 0; i < circles.size(); i++) {
+      cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+
+      ROS_INFO_STREAM("Radius:" << radius);
+
+      // circle center
+      //cv::circle(cv_circlesImg.image, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
+      // circle outline
+      cv::circle(cv_circlesImg.image, center, radius, 0, 1, 8, 0);
+     }
+
+     Canny(cv_circlesImg.image, cv_circlesImg.image, 500, 200);//just applied canny for visibility
+   */
+
+
+  image_pub.publish(cv_img.toImageMsg());
+  detected_pub.publish(cv_detectionImg.toImageMsg());
+}
+
+void mapToImage() {
   int size_x = map.info.width;
   int size_y = map.info.height;
 
@@ -77,8 +121,9 @@ void detectTrees() {
     *map_mat = cv::Mat(size_y, size_x, CV_8U);
   }
 
-  cv_circlesImg.image = cv::Mat(size_y, size_x, CV_8U, 255);
-
+  if ((cv_detectionImg.image.rows != size_y) && (cv_detectionImg.image.cols != size_x)) {
+    cv_detectionImg.image = cv::Mat(size_y, size_x, CV_8U, 255);
+  }
 
   const std::vector<int8_t>& map_data(map.data);
 
@@ -110,42 +155,6 @@ void detectTrees() {
       }
     }
   }
-
-  // Reduce the noise so we avoid false circle detection
-  //cv::GaussianBlur(cv_circlesImg.image, cv_img.image, cv::Size(9, 9), 2, 2);
-/*
-   std::vector<cv::Vec3f> circles;
-
-   // Apply the Hough Transform to find the circles
-   HoughCircles(cv_img.image,
-               circles,
-               CV_HOUGH_GRADIENT,
-               inverse_ratio_of_resolution,
-               minimum_distance_between_detected_centers,
-               upper_threshold_for_canny_detector,
-               center_detection_threshold,
-               min_radius,
-               max_radius);
-
-   std::cout << "\nNo:" << circles.size() << " circles" << std::endl;
-
-   // Draw the circles detected
-   for (size_t i = 0; i < circles.size(); i++) {
-    cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-    int radius = cvRound(circles[i][2]);
-
-    ROS_INFO_STREAM("Radius:" << radius);
-
-    // circle center
-    //cv::circle(cv_circlesImg.image, center, 3, cv::Scalar(0, 255, 0), -1, 8, 0);
-    // circle outline
-    cv::circle(cv_circlesImg.image, center, radius, 0, 1, 8, 0);
-   }
-
-   Canny(cv_circlesImg.image, cv_circlesImg.image, 500, 200);//just applied canny for visibility
- */
-  image_pub.publish(cv_img.toImageMsg());
-  circles_pub.publish(cv_circlesImg.toImageMsg());
 
   return;
 }
