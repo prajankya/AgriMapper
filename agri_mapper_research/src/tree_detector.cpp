ad6865c69bd7abeb877d870e22c2563f6e18a970
Prajankya <prajankya@gmail.com>
2017-01-19 17:04:17 +0530

    Working code, dimension is wrong

--- agri_mapper_research/src/tree_detector.cpp
+++ agri_mapper_research/src/tree_detector.cpp
@@ -4,6 +4,7 @@
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/Image.h>
 #include <nav_msgs/OccupancyGrid.h>
+#include "opencv2/opencv.hpp"
 
 void mapSubCallback(const nav_msgs::OccupancyGridConstPtr& map);
 void detectTrees();
@@ -53,7 +54,7 @@ int main(int argc, char **argv) {
     ros::spinOnce();
 
     if (map.header.frame_id != "") {
-      detectTrees();
+      //detectTrees();
     }
 
     rate.sleep();
@@ -75,6 +76,49 @@ void detectTrees() {
     cv_detectionImg.image = cv_img.image;
   }
 
+  // Setup SimpleBlobDetector parameters.
+  cv::SimpleBlobDetector::Params params;
+
+  // Change thresholds
+  //params.minThreshold = 1;
+  //params.maxThreshold = 200;
+
+  // Filter by Area.
+  // params.filterByArea = true;
+  // params.minArea = 1500;
+
+  // Filter by Circularity
+  // params.filterByCircularity = true;
+  // params.minCircularity = 0.1;
+
+  // Filter by Convexity
+  // params.filterByConvexity = true;
+  // params.minConvexity = 0.87;
+
+  // Filter by Inertia
+  // params.filterByInertia = true;
+  // params.minInertiaRatio = 0.01;
+
+
+  // Storage for blobs
+  std::vector<cv::KeyPoint> keypoints;
+
+
+  // Set up detector with params
+  cv::Ptr<cv::SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+
+  // Detect blobs
+  detector->detect(cv_detectionImg.image, keypoints);
+
+  // Draw detected blobs as red circles.
+  // DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures
+  // the size of the circle corresponds to the size of blob
+
+  cv::Mat im_with_keypoints;
+  cv::drawKeypoints(cv_detectionImg.image, keypoints, im_with_keypoints, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+  //cv_detectionImg.image = im_with_keypoints;
+  // Show blobs
+  imshow("keypoints", im_with_keypoints);
   /*
      std::vector<cv::Vec3f> circles;
 
